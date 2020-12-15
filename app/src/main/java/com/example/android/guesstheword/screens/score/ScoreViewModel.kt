7505d29dccd5987a0df8d9c421a47e7654b34b34
Maleina Bidek <maleina@ucla.edu>
2020-12-15 15:00:56 -0500

    refactor: Remove unnecessary imports

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -3,7 +3,6 @@ package com.example.android.guesstheword.screens.score
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.navigation.fragment.findNavController
 
 class ScoreViewModel(finalScore: Int) : ViewModel() {
 
